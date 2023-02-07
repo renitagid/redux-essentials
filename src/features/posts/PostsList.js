f120e8861654ca9deeb81a8985fcf7dfd3522f99
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-06 17:37:49 -0800

    added more functionality to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+import {ReactionButtons} from './ReactionButton'
 
 
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = posts.map(post => (
+  const renderedPosts = orderedPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
+      <PostAuthor userId={post.user} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+      <ReactionButtons />
     </article>
   ))
 
+  
   return (
     <section className="posts-list">
       <h2>Posts</h2>
