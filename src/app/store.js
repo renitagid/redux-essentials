316a7c617e961e7f85bbc42d5bd25767fe070cc8
renitagid <renita.y.ip@gmail.com>
2023-02-06 16:00:34 -0800

    completed tutorial to part 3

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '../features/posts/postsSlice'
+
+
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postsReducer
+  }
 })
